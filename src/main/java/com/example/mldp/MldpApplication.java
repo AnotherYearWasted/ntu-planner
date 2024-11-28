f805ee566c713d1b1580b5b7d9e6f66a72abbb39
centroidissue <iamnoob2210@gmail.com>
2024-11-28 15:34:14 +0800

    Initial commit

---
+++ src/main/java/com/example/mldp/MldpApplication.java
@@ -0,0 +1,13 @@
+package com.example.mldp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MldpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MldpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mldp/MldpApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mldp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MldpApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
